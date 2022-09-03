be372a7d34cf5017b63c791c75b5bfa2bd8131f1
pram0d1 <surya.promi@gmail.com>
2022-09-03 16:51:41 +0530

    varaible number added

--- variables.js
+++ variables.js
@@ -2,7 +2,7 @@ var a = "12";
 var b = "20";
 
 if (a>b){
-    console.log("a is greater");
+    console.log("a is greater number");
 }else{
-    console.log("b is greater");
+    console.log("b is greater number");
 }
