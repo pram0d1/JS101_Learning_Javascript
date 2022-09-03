be014a20948c2fb3c04d2468e670c0d39ea7fd92
pram0d1 <surya.promi@gmail.com>
2022-09-03 17:11:37 +0530

    caps

--- variables.js
+++ variables.js
@@ -2,7 +2,7 @@ var a = "12";
 var b = "20";
 
 if (a>b){
-    console.log("a is greater number");
+    console.log("A is Greater Number");
 }else{
-    console.log("b is greater number");
+    console.log("B is Greater Number");
 }
