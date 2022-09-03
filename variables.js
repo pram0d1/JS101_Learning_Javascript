be87c086adbee441cbc3436328bd472e2a9fa02a
pram0d1 <surya.promi@gmail.com>
2022-09-03 17:54:31 +0530

    ! commmit !

--- variables.js
+++ variables.js
@@ -2,7 +2,7 @@ var a = "12";
 var b = "20";
 
 if (a>b){
-    console.log("A is Greater Number");
+    console.log("A is Greater Number!");
 }else{
-    console.log("B is Greater Number");
+    console.log("B is Greater Number!");
 }
